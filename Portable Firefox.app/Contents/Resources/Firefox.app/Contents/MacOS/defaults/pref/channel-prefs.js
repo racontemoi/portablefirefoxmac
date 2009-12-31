41f221e4e154f0f748a1978cf8f88403f4a92522
Denis Rosset (racontemoi.ch) <dr@racontemoi.ch>
2009-12-31 16:19:01 +0100

    Import of Portable Firefox OS X/3.5 r4.0 English.

---
+++ Portable Firefox.app/Contents/Resources/Firefox.app/Contents/MacOS/defaults/pref/channel-prefs.js
@@ -0,0 +1,2 @@
+//@line 2 "/builds/moz2_slave/macosx_build/build/browser/app/profile/channel-prefs.js"
+pref("app.update.channel", "release");
---
+++ Portable Firefox.app/Contents/Resources/Firefox.app/Contents/MacOS/defaults/pref/firefox-l10n.js
@@ -0,0 +1,5 @@
+//@line 36 "/builds/moz2_slave/macosx_build/build/browser/locales/en-US/firefox-l10n.js"
+
+//@line 38 "/builds/moz2_slave/macosx_build/build/browser/locales/en-US/firefox-l10n.js"
+
+pref("general.useragent.locale", "en-US");
